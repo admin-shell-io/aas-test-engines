2051a7c33d4bb181b015ceef6e3e3fc026e25616
Björn Otto <bjoern.otto@ifak.eu>
2024-02-01 00:03:38 +0100

    cli: allow checking of json and xml files

--- aas_test_engines/__main__.py
+++ aas_test_engines/__main__.py
@@ -1,20 +1,39 @@
 import argparse
 import sys
 from aas_test_engines import api, file
+from enum import Enum
 
+class Formats(Enum):
+    xml = 'xml'
+    json = 'json'
+    aasx = 'aasx'
+
+    def __str__(self):
+        return self.value
 
 def run_file_test(argv):
-    parser = argparse.ArgumentParser(description='Runs test cases')
+    parser = argparse.ArgumentParser(description='Checks a file for compliance with the AAS meta-model')
     parser.add_argument('file',
                         type=argparse.FileType('r'),
                         help='the file to check')
+    parser.add_argument('--format',
+                        type=Formats,
+                        default=Formats.aasx,
+                        choices=list(Formats))
     args = parser.parse_args(argv)
-    result = file.check_aasx_file(args.file)
-    print(result)
+    if args.format == Formats.aasx:
+        result = file.check_aasx_file(args.file)
+    elif args.format == Formats.json:
+        result = file.check_json_file(args.file)
+    elif args.format == Formats.xml:
+        result = file.check_xml_file(args.file)
+    else:
+        raise Exception(f"Invalid format {args.format}")
+    result.dump()
 
 
 def run_api_test(argv):
-    parser = argparse.ArgumentParser(description='Runs test cases')
+    parser = argparse.ArgumentParser(description='Checks a server instance for compliance with the AAS api')
     parser.add_argument('server',
                         type=str,
                         help='server to run the tests against')
