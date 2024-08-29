501704e537a6b6b6d20480527614891a09001070
Björn Otto <bjoern.otto@ifak.eu>
2024-08-29 15:46:04 +0200

    Add check_sdks script

---
+++ bin/check_sdks.py
@@ -0,0 +1,89 @@
+#! /usr/bin/env python3
+
+import os
+from aas_test_engines import file
+import json
+import subprocess
+from fences.core.util import ConfusionMatrix
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+root_dir = os.path.realpath(os.path.join(script_dir, "check_sdks"))
+
+test_data_dir = os.path.join(root_dir, "test_data")
+valid_samples_dir = os.path.join(test_data_dir, "valid")
+invalid_samples_dir = os.path.join(test_data_dir, "invalid")
+
+
+def is_non_empty(directory: str) -> bool:
+    return os.path.exists(directory) and os.listdir(directory)
+
+
+print("--- Generate Test Data ---")
+num_valid = 0
+num_invalid = 0
+
+if is_non_empty(test_data_dir):
+    print(f"{test_data_dir} already exists, skipping sample generation")
+    for f in os.listdir(test_data_dir):
+        if f.startswith('i'):
+            num_invalid += 1
+        else:
+            num_valid += 1
+else:
+    print(f"Writing samples to {test_data_dir}...")
+    os.mkdir(test_data_dir)
+
+    for is_valid, sample in file.generate():
+        if is_valid:
+            with open(os.path.join(test_data_dir, f"v{num_valid}.json"), "w") as f:
+                json.dump(sample, f, indent=4)
+            num_valid += 1
+        else:
+            with open(os.path.join(test_data_dir, f"i{num_invalid}.json"), "w") as f:
+                json.dump(sample, f, indent=4)
+            num_invalid += 1
+
+        if (num_valid + num_invalid) % 100 == 0:
+            print(f"#Valid:   {num_valid}")
+            print(f"#Invalid: {num_invalid}")
+
+    print("Done.")
+
+print(f"#Valid:   {num_valid}")
+print(f"#Invalid: {num_invalid}")
+print()
+
+sdks = [
+    'aas_core_csharp'
+]
+
+print("--- Execute SDKs ---")
+for sdk in sdks:
+    print(f"Executing {sdk}...")
+    sdk_dir = os.path.join(root_dir, "sdks", sdk)
+    out_dir = os.path.join(root_dir, "out", sdk)
+
+    if is_non_empty(out_dir):
+        print(f"{out_dir} already exists, reusing last results")
+    else:
+        subprocess.check_call([
+            "docker", "compose",
+            "-f", os.path.join(sdk_dir, "docker-compose.yml"),
+            "run",
+            "--remove-orphans",
+            "app"
+        ], cwd=sdk_dir)
+print()
+
+print("--- Analyse Results ---")
+for sdk in sdks:
+    mat = ConfusionMatrix()
+    out_dir = os.path.join(root_dir, "out", sdk)
+    for file in os.listdir(out_dir):
+        if file.startswith('i'):
+            mat.invalid_accepted += 1
+        else:
+            mat.valid_accepted += 1
+    mat.valid_rejected = num_valid - mat.valid_accepted
+    mat.invalid_rejected = num_invalid - mat.invalid_accepted
+    mat.print()
---
+++ bin/check_sdks/sdks/aas_core_csharp/src/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Text.Json.Nodes;
+
+using Aas = AasCore.Aas3_0;
+using AasJsonization = AasCore.Aas3_0.Jsonization;
+using AasVerification = AasCore.Aas3_0.Verification;
+
+public class Program
+{
+    static String[] blacklist = new String[] {
+        "Message broker must be a model reference to a referable.",
+        "Max. interval is not applicable for input direction.",
+        "Observed must be a model reference to a referable.",
+        "Derived-from must be a model reference to an asset administration shell.",
+        "All submodels must be model references to a submodel.",
+        "Constraint AASc-3a-009: If data type is a an integer, real or rational with a measure or currency, unit or unit ID shall be defined.",
+    };
+
+    public static String? Check(string file, string out_file)
+    {
+        string text = File.ReadAllText(file);
+        var options = new System.Text.Json.JsonDocumentOptions();
+        options.MaxDepth = 1000;
+        var jsonNode = JsonNode.Parse(text, null, options);
+
+        if(jsonNode == null)
+        {
+            return null;
+        }
+
+        Aas.Environment environment;
+        try
+        {
+            environment = AasJsonization.Deserialize.EnvironmentFrom(jsonNode);
+        }
+        catch(AasCore.Aas3_0.Jsonization.Exception e)
+        {
+            return null;
+        }
+
+        foreach (var error in AasVerification.Verify(environment))
+        {
+            if(!blacklist.Any(error.Cause.Contains))
+            {
+                return null;
+            }
+        }
+
+        // deserialize
+        var jsonString = AasJsonization.Serialize.ToJsonObject(environment).ToString();
+        File.WriteAllText(out_file, jsonString);
+        return jsonString;
+    }
+
+    public static void Main()
+    {
+        string[] files = Directory.GetFiles("/test_data");
+        foreach(string file in files)
+        {
+            var result = Check(file, $"/out/{Path.GetFileName(file)}");
+        }
+    }
+}
